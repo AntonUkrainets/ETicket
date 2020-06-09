77a85e9b42bc07202812bc3c3415b14ca631a129
Anton <anton.ukrainets@gmail.com>
2020-06-10 00:37:24 +0300

    Add UtcToLocalDateTimeConverter

---
+++ ETicketMobile/ETicketMobile.UserInterface/BindingConverters/UtcToLocalDateTimeConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace ETicketMobile.UserInterface.BindingConverters
+{
+    public class UtcToLocalDateTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is DateTime)
+                return ((DateTime)value).ToLocalTime();
+            else
+                return DateTime.Parse(value?.ToString()).ToLocalTime();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
